c9e1f40adef5f2a5350ccd059502d4df527fbbca
simohauml <simo.hauml@gmail.com>
2018-01-17 20:36:45 +0800

    exercise 36 update

--- chapter1/1.36.rkt
+++ chapter1/1.36.rkt
@@ -2,11 +2,26 @@
 
 (define number-elements
   (lambda (lst)
-    (if (null? lst) '()
-        (g (list 0 (car lst)) (number-elements (cdr lst))))))
+    (if (null? lst)
+        '()
+        (g (list 0 (car lst))
+           (number-elements (cdr lst))))))
 
 (define g
   (lambda (num-and-sexp lst)
     (if (null? lst)
         (list num-and-sexp)
-        (cons num-and-sexp (cons (+ 1 (car num-and-sexp)) (cdr lst))))))
+        (letrec ((loop (lambda (lst)
+                         (cond
+                           ((null? lst) '())
+                           (else
+                            (cons (cons (+ 1 (caar lst))
+                                        (cdar lst))
+                                  (loop (cdr lst))))))))
+          (cons num-and-sexp
+                (loop lst))))))
+
+;; test
+(define l1 '(a b c d e f g))
+(define l2 '((0 a) (1 b) (2 c) (3 d)))
+(number-elements l1)
